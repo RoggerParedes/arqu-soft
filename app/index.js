4a0670427d0c87a5792e4a2b6a16d2c90560ad0d
RoggerParedes <rparedest@fi.uba.ar>
2022-09-29 16:40:43 -0500

    Se instala axios Se crean los metodos para los testeos

--- app/index.js
+++ app/index.js
@@ -1,15 +1,33 @@
-const express = require('express');
+import express from 'express';
+import axios from 'axios';
 
 const PORT = 3000;
-
+const TIMEOUT = 5*1000;
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send(`Conectados al puerto ${PORT}`);
+    res.send("Ping");
 });
 
-app.get('/arqsoft/bbox:202202.1',(req, res) => {
-    res.send(`Conectados al puerto ${PORT}`);
+app.get('/bbox_async',async (req, res) => {
+    axios.get('http://bbox:9090/').then( response => {
+        res.status(200).send(`${response.data} \n`);
+    }).catch( error => {
+        console.log(`ERROR: ${error}`);
+    });
 })
 
-app.listen(PORT,() => {console.log("Escuchando en el puerto",PORT)});
+app.get('/bbox_sync',async (req, res) => {
+    axios.get('http://bbox:9091/').then( response => {
+        res.status(200).send(`${response.data} \n`);
+    }).catch( error => {
+        console.log(`ERROR: ${error}`);
+    });
+})
+
+app.get('/heavy',async (req, res) => {
+    for(let t = new Date();new Date() - t < TIMEOUT;);
+    res.status(200).send('Heavy Hello World \n')
+})
+
+app.listen(PORT,() => {console.log(`Conectados adl puerto ${PORT} \n`)});
