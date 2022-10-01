b8e3f8f8382f643fb8d6be18ced4328821fa8fa2
RoggerParedes <rparedest@fi.uba.ar>
2022-10-01 09:13:20 -0500

    Se levanta artillery

--- app/index.js
+++ app/index.js
@@ -9,22 +9,6 @@ app.get('/',(req, res) => {
     res.send("Ping");
 });
 
-app.get('/bbox_async',async (req, res) => {
-    axios.get('http://bbox:9090/').then( response => {
-        res.status(200).send(`${response.data} \n`);
-    }).catch( error => {
-        console.log(`ERROR: ${error}`);
-    });
-})
-
-app.get('/bbox_sync',async (req, res) => {
-    axios.get('http://bbox:9091/').then( response => {
-        res.status(200).send(`${response.data} \n`);
-    }).catch( error => {
-        console.log(`ERROR: ${error}`);
-    });
-})
-
 app.get('/heavy',async (req, res) => {
     for(let t = new Date();new Date() - t < TIMEOUT;);
     res.status(200).send('Heavy Hello World \n')
