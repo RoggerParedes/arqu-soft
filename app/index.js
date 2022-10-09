3ed24e41fd0da094579f6066b44e20588a798d3b
RoggerParedes <rparedest@fi.uba.ar>
2022-10-08 19:25:50 -0500

    Avance cn metricas, y generacion ed id

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import axios from 'axios';
+import {v4 as uuidv4} from 'uuid'
 
 const PORT = 3000;
 const TIMEOUT = 5*1000;
 const app = express();
 const end_point = 'http://bbox:';
-const port_async = '9091';
-const port_sync = '9090';
+const port_async = '9090';
+const port_sync = '9091';
+const uuid_ = uuidv4();
 
 app.get('/',(req, res) => {
-    res.send("Ping \n");
+    res.send(`Ping token: ${uuid_} \n`);
 });
 
 app.get('/heavy',async (req, res) => {
@@ -19,14 +21,14 @@ app.get('/heavy',async (req, res) => {
 
 app.get('/async',async (req, res) => {
     axios.get(`${end_point}${port_async}`).then( response => {
-        res.status(200).send(`Response Async Data: ${response.data} \n`);
+        res.status(200).send(`Response Async Data: ${response.data} token: ${uuid_} \n`);
     });
 });
 
 app.get('/sync',async (req, res) => {
     axios.get(`${end_point}${port_sync}`).then( response => {
-        res.status(200).send(`Response sync Data: ${response.data} \n`);
+        res.status(200).send(`Response sync Data: ${response.data} token: ${uuid_} \n`);
     });
 });
 
-app.listen(PORT,() => {console.log(`Conectados al puerto ${PORT} \n`)});
+app.listen(PORT,() => {console.log(`Conectados al puerto ${PORT}  token: ${uuid_} \n`)});
